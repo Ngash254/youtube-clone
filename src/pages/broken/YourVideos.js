a2ed79cd4deca81fd15ed69af4821a297d3a52e2
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-08-08 18:42:22 +0530

    Add some gifs and fix responsiveness

--- src/pages/broken/YourVideos.js
+++ src/pages/broken/YourVideos.js
@@ -1,23 +1,29 @@
 import React from "react";
 import "./YourVideos.css";
 import { Link } from "react-router-dom";
+import video from "./Videos/video-2.mp4";
 
 function YourVideos() {
     return (
         <div className="yourVideos">
             <div className="main__container">
-                <h1>Sorry! This page is out of service...</h1>
-                <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Eveniet perspiciatis ullam harum repudiandae id a quia aut
-                    eum hic nihil obcaecati tenetur iste, pariatur minus dolore
-                    qui et similique. Numquam!
-                </p>
-                <div className="video-div">
-                    <video height="304" width="560" controls muted loop>
-                        <source src="./videos/some.mp4" type="video/mp4" />
-                        Your browser does not support video
-                    </video>
+                <div className="cont__title">
+                    <h1>Sorry! This page is out of service...</h1>
+                </div>
+                <div className="cont__paragraph">
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Eveniet repudiandae id a quia aut. Numquam!
+                    </p>
+                </div>
+
+                <div className="cont__video">
+                    <div className="video-div">
+                        <video height="304" width="560" autoPlay muted loop>
+                            <source src={video} type="video/mp4" />
+                            Your browser does not support video
+                        </video>
+                    </div>
                 </div>
 
                 <div className="home__button__div">

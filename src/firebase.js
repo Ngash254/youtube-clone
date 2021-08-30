32f7f2a4d84a5d9c5ca6fa6741a6d736aed617bb
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-08-30 18:26:47 +0530

    Add firebase and redux packages to the project

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAM5of4YDn9xBg5DsQIBuESRyo4O19BGvw",
+    authDomain: "yt-clone-1001.firebaseapp.com",
+    projectId: "yt-clone-1001",
+    storageBucket: "yt-clone-1001.appspot.com",
+    messagingSenderId: "990076614837",
+    appId: "1:990076614837:web:b854afe19b5e54d62166fc",
+    measurementId: "G-HT63L3K0M0"
+};
+
+firebase.initializeApp(firebaseConfig);

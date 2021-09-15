603f9a3895168aa087ac5135a64e3b0ce905be76
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-09-15 07:28:34 +0530

    Add home videos fetching action types

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,4 +1,13 @@
+// types for the authentication 
+
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILED = "LOGIN_FAILED";
 export const LOGIN_REQUEST = "LOGIN_REQUEST";
 export const LOAD_PROFILE = "LOAD_PROFILE";
+
+
+//types for the videos
+
+export const HOME_VIDEOS_SUCCESS = "HOMEVIDEOS_SUCCESS";
+export const HOME_VIDEOS_FAILED = "HOMEVIDEOS_FAILED";
+export const HOME_VIDEOS_REQUEST = "HOMEVIDEOS_REQUEST";

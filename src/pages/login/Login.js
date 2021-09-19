a6b4a0872adfbf5d7bb9046fea52797fcc9be130
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-09-19 12:09:15 +0530

    Comment deletion

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -13,21 +13,6 @@ const Login = () => {
         dispatch(loginFn());
     }
 
-    /*
-        I am commenting out the following code because for some reason
-        the history hook wont work. will get back to this some time later 
-    
-        const accessToken = useSelector(state => state.auth.accessToken);
-
-        const history = useHistory();
-
-        useEffect(() => {
-            if(accessToken) {
-                console.log(accessToken);
-                history.push("/");
-            }
-        }, [accessToken, history]);
-    */
     const accessToken = useSelector(state => state.auth.accessToken);
     
     useEffect(() => {
@@ -42,6 +27,7 @@ const Login = () => {
     return (
         <div className="Login">
             <div className="login__main">
+                <h1>Youtube Clone</h1>
                 <div className="img__div">
                     <img className="login__img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3GrZtppm70a8zwq4O5ZuDGR92pWNcVjP7hA&usqp=CAU" alt="yt-logo"></img>
                 </div>

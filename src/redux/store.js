dad1a3cc4b6d2ee3f39a41ba5ba6c5eaa23fea1c
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-11-16 12:28:53 +0530

    Add selectedVideo and channelDetails reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,14 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { authReducer } from "./reducers/authReducer";
-import { homeVideosReducer } from "./reducers/videosReducer";
+import { channelReducer } from "./reducers/channelReducer";
+import { homeVideosReducer, selectedVideoReducer } from "./reducers/videosReducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    homeVideos: homeVideosReducer
+    homeVideos: homeVideosReducer,
+    selectedVideo: selectedVideoReducer,
+    channelDetails: channelReducer,
 })
 
 const store = createStore(

6517e502e40215d9fe4095acdeb1721e45806899
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-12-01 18:40:25 +0530

    Create setting status action

--- src/redux/actions/channelAction.js
+++ src/redux/actions/channelAction.js
@@ -1,4 +1,4 @@
-import { CHANNEL_DETAILS_FAILED, CHANNEL_DETAILS_REQUEST, CHANNEL_DETAILS_SUCCESS } from "../actionTypes"
+import { CHANNEL_DETAILS_FAILED, CHANNEL_DETAILS_REQUEST, CHANNEL_DETAILS_SUCCESS, SET_SUBSCRIPTION_STATUS, SUBSCRIPTION_STATUS_CHECK_SUCCESS } from "../actionTypes"
 import request from "../../api";
 
 export const getChannelDetails = id => async dispatch => {
@@ -25,5 +25,32 @@ export const getChannelDetails = id => async dispatch => {
             type: CHANNEL_DETAILS_FAILED,
             payload: error.message
         })
+    }
+}
+
+//action to check a user's subscription status
+export const checkUserSubscriptionStatus = id => async (dispatch, getState) => {
+    try {
+
+        const { data } = await request("/subscriptions", {
+            params: {
+                part: "contentDetails, snippet",
+                mine: true,
+                forChannelId: id
+            },
+            headers: {
+                Authorization: `Bearer ${getState().auth.accessToken}`
+            }
+        })
+
+        dispatch({
+            type: SET_SUBSCRIPTION_STATUS,
+            payload: data.items.length !== 0
+        })
+        console.log(data);
+
+
+    } catch (error) {
+        console.log(error.message)
     }
 }

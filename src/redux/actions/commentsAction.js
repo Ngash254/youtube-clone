487538d05667267b2f2426795a49620abfb8e6d1
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-12-02 09:16:34 +0530

    Action creator for inserting comments

--- src/redux/actions/commentsAction.js
+++ src/redux/actions/commentsAction.js
@@ -1,5 +1,5 @@
 import request from "../../api"
-import { COMMENT_THREAD_FAILED, COMMENT_THREAD_REQUEST, COMMENT_THREAD_SUCCESS } from "../actionTypes"
+import { COMMENT_THREAD_FAILED, COMMENT_THREAD_REQUEST, COMMENT_THREAD_SUCCESS, INSERT_COMMENT_FAILED, INSERT_COMMENT_SUCCESS } from "../actionTypes"
 
 export const getCommentThreads = id => async (dispatch) => {
     try {
@@ -19,10 +19,48 @@ export const getCommentThreads = id => async (dispatch) => {
             payload: data.items,
         })
 
-
     } catch (error) {
         dispatch({
             type: COMMENT_THREAD_FAILED,
+            payload: error.message
+        })
+    }
+}
+
+export const insertComment = (id, text) => async (dispatch, getState) => {
+    try {
+        const userData = {
+            snippet : {
+                videoId: id,
+                topLevelComment: {
+                    snippet: {
+                        textOriginal: text
+                    }
+                }
+            }
+        }
+
+        await request.post("/commentThreads", userData, {
+            params: {
+                part: "snippet",
+            },
+            headers: {
+                Authorization: `Bearer ${getState().auth.accessToken}`
+            }
+        })
+
+        dispatch({
+            type: INSERT_COMMENT_SUCCESS,
+        })
+
+        //after the comment has been added, refresh the commentThread in a setTimeout
+        setTimeout(() => dispatch(getCommentThreads(id)), 3000)
+
+
+    } catch (error) {
+        console.log(error)
+        dispatch({
+            type: INSERT_COMMENT_FAILED,
             payload: error.message
         })
     }

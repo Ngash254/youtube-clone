dcb92f45ef8e5b4bf5955cbb78414a7f0e4d4464
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-12-02 06:40:03 +0530

    Configure actual properties and scrap off dislikes count

--- src/pages/watchVideo/CommentsBar.js
+++ src/pages/watchVideo/CommentsBar.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./CommentsBar.css";
 import { Avatar } from "@material-ui/core";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
-import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import moment from "moment";
+import numeral from "numeral";
 
 function CommentsBar({
     avatarImage,
@@ -11,7 +12,6 @@ function CommentsBar({
     commentTimestamp,
     comment,
     commentLikes,
-    commentDislikes,
 }) {
     return (
         <div className="CommentsBar">
@@ -25,18 +25,14 @@ function CommentsBar({
             <div className="comments__details">
                 <div className="name__and__timestamp">
                     <span className="user__name">{userName}</span>
-                    <span className="timestamp">{commentTimestamp}</span>
+                    <span className="timestamp">{moment(commentTimestamp).fromNow()}</span>
                 </div>
                 <div className="comment__space">
                     <span className="user__comment">{comment}</span>
                 </div>
                 <div className="comments__section__icons">
                     <p>
-                        <ThumbUpIcon className="thumbUp__icon" /> {commentLikes}{" "}
-                    </p>
-                    <p>
-                        <ThumbDownIcon className="thumbDown__icon" />{" "}
-                        {commentDislikes}{" "}
+                        <ThumbUpIcon className="thumbUp__icon" /> {numeral(commentLikes).format("0.a")}{" "}
                     </p>
                     <span>REPLY</span>
                 </div>

edb4845622fed4337da99793e62ffbea6c2f4393
Ng'ash_254 <samuelnganga.w@gmail.com>
2021-12-16 14:00:17 +0530

    Create a comment replies reducer

--- src/redux/reducers/commentsReducer.js
+++ src/redux/reducers/commentsReducer.js
@@ -1,4 +1,4 @@
-import { COMMENT_THREAD_FAILED, COMMENT_THREAD_REQUEST, COMMENT_THREAD_SUCCESS } from "../actionTypes";
+import { COMMENT_REPLIES_FAILED, COMMENT_REPLIES_REQUEST, COMMENT_REPLIES_SUCCESS, COMMENT_THREAD_FAILED, COMMENT_THREAD_REQUEST, COMMENT_THREAD_SUCCESS } from "../actionTypes";
 
 const initialState = {
     loading: false,
@@ -30,3 +30,38 @@ export const commentsReducer = (state={initialState}, action) => {
             return state
     }
 }
+
+export const commentRepliesReducer = (
+    state={
+        loading: false,
+        replies: []
+    },
+    action
+) => {
+    const {type, payload} = action;
+
+    switch(type) {
+        case COMMENT_REPLIES_REQUEST: 
+            return {
+                ...state,
+                loading: true,
+            };
+
+        case COMMENT_REPLIES_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                replies: payload.items,
+            }
+
+        case COMMENT_REPLIES_FAILED:
+            return {
+                ...state,
+                loading: false,
+                replies: null
+            }
+        default: 
+            return state;
+    }
+}
+

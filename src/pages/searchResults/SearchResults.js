fdc5b231787f2a2d1e68eebeae7ee64037364680
Ng'ash_254 <samuelnganga.w@gmail.com>
2022-01-20 20:08:46 +0530

    Add the infinite scroll component

--- src/pages/searchResults/SearchResults.js
+++ src/pages/searchResults/SearchResults.js
@@ -1,30 +1,58 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./SearchResults.css";
 import TuneIcon from "@material-ui/icons/Tune";
 import SearchResultsComponent from "../../Components/searchResults/SearchResultsComponent";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { getVideosByUserSearchInput } from "../../redux/actions/videosAction";
 
 function SearchResults() {
+    const [userInput, setUserInput] = useState("");
+    const dispatch = useDispatch();
 
     //get the array of videos from the store;
-    const {videos, loading} = useSelector(state => state.searchResults);
+    const {videos} = useSelector(state => state.searchResults);
+
+    //grab the search input as was
+    useEffect(() => {
+        const userInputElement = document.getElementById("user-input");
+        setUserInput(
+            userInputElement 
+                ? userInputElement.value.length === 0 ? "" 
+                : userInputElement.value 
+            : "");
+    }, [userInput]);
+
+    const fetchMoreSearchResults = () => {
+        //dispatch the getsearch results action creator
+        dispatch(getVideosByUserSearchInput(userInput));
+    }
 
     return (
-        <div className="searchResults">
+        <div className="searchResults" id="search-results-page">
             <div className="searchResults__top">
                 <TuneIcon />
                 <h2>FILTER</h2>
             </div>
 
             <div className="searchResults__bottom">
-                {!loading && videos?.map((result) => (
-                    
-                    <SearchResultsComponent
-                        video={result}
-                        key={result.id}
-                    />
-                    
-                ))}
+                <InfiniteScroll
+                    dataLength={videos ? videos.length : 12}
+                    next={fetchMoreSearchResults}
+                    hasMore={true}
+                    loader={<h4>Fetching videos...</h4>}
+                    scrollableTarget="search-results-page"
+                    style={{ display: 'flex', flexDirection: 'column' }}
+                >
+                    {videos?.map((result) => (
+                        
+                        <SearchResultsComponent
+                            video={result}
+                            key={result.id}
+                        />
+                        
+                    ))}
+                </InfiniteScroll>
             </div>
         </div>
     );

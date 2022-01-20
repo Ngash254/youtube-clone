34a5310c3bd15fa6364f88cb16515b5953dab0d2
Ng'ash_254 <samuelnganga.w@gmail.com>
2022-01-20 16:25:18 +0530

    Deletions on unused code lines

--- src/pages/searchResults/SearchResults.js
+++ src/pages/searchResults/SearchResults.js
@@ -1,14 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./SearchResults.css";
 import TuneIcon from "@material-ui/icons/Tune";
 import SearchResultsComponent from "../../Components/searchResults/SearchResultsComponent";
-import {useDispatch} from "react-redux";
-import {getVideosByUserSearchInput} from "../../redux/actions/videosAction";
 import { useSelector } from "react-redux";
 
 function SearchResults() {
-
-    const dispatch = useDispatch()
 
     //get the array of videos from the store;
     const {videos, loading} = useSelector(state => state.searchResults);
